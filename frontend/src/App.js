3483e6925b2739b4893ee60982901f09e28f2670
Rishabh Agarwal <rishabh.garwal@habilelabs.io>
2022-02-26 14:15:04 +0530

    fix: added Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,39 @@
-import DashboardContent from './Dashboard'
+import React, { useState } from 'react';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import DashboardContent from './Dashboard';
 import Home from './Home';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 const App = () => {
-  return (
-    <div className="App">
-      <DashboardContent>
-        <Home />
-      </DashboardContent>
-    </div>
+  const history = createBrowserHistory();
+  // localStorage.setItem("token", "abcd");
+  const [isAuthenticate, setIsAuthenticate] = useState(localStorage.getItem('token') ? true : false);
+
+return (
+  <Router history={history}>
+      {isAuthenticate ? (
+          <Box>
+            <Box component="main">
+              <Routes>
+                <Route exact path="/" element={
+                  <DashboardContent>
+                    <Home />
+                  </DashboardContent>} />
+              </Routes>
+            </Box>
+          </Box>
+      ) : (
+          <Box component="main">
+              <Routes>
+                <Route exact path="/login" element={<SignIn />} />
+                <Route exact path="/signup" element={<SignUp />} />
+              </Routes>
+          </Box>
+      )}
+  </Router>
   );
 }
 
